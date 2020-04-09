8670f5fcb95d9c91321a17b30c2e1e9669078ca5
Waqas D <wdaimee@gmail.com>
2020-04-09 19:34:23 -0400

    added boilerplate to views.py and placeholder home route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the Magic The Gathering Card Collection and Deck Builder!")
