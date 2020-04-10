5b0525b15f0a310dcba0b9639b0149a5a9939a28
Waqas D <wdaimee@gmail.com>
2020-04-09 20:38:13 -0400

    added static files: style.css/main.js and added collapsable navbar with bootstrap to base.html

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Magic The Gathering Card Collection and Deck Builder!")
+    return render(request, 'base.html')
