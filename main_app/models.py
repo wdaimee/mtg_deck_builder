3de0e0e1332ded7dfa7c03f55f01a63853a5fc6f
Waqas D <wdaimee@gmail.com>
2020-04-09 21:51:16 -0400

    made models for color, card, and deck in models.py and made migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+CARDS = (
+    ('L', 'Land'),
+    ('C', 'Creature'),
+    ('A', 'Artifact'),
+    ('E', 'Enchantment'),
+    ('P', 'Planeswalker'),
+    ('I', 'Instant'),
+    ('S', 'Sorcery')
+)
+
+class Color(models.Model):
+    color = models.CharField(max_length=100)
+
+class Card(models.Model):
+    name = models.CharField(max_length=200)
+    cmc = models.IntegerField()
+    card_type = models.CharField(
+        max_length = 1,
+        choices = CARDS,
+        default=CARDS[0][0]
+    )
+    ability = models.CharField(max_length=250)
+    colors = models.ManyToManyField(Color)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Deck(models.Model):
+    name = models.CharField(max_length=100)
+    commander = models.CharField(max_length=100)
+    cards = models.ManyToManyField(Card)
+
